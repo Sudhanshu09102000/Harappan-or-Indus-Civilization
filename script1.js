211707e6ab42a92e30b60946d26237f340e0fe26
Sudhanshu09102000 <127600086+Sudhanshu09102000@users.noreply.github.com>
2025-11-18 17:30:01 +0530

    Add files via upload

---
+++ script1.js
@@ -0,0 +1,135 @@
+const questions= {
+    // Questions
+
+1: {
+text: "Who was the founder of the Haryanka dynasty?",
+options: ["Bimbisara", "Ajatashatru", "Mahapadma Nanda", "Chandragupta Maurya"],
+correct: "1",
+image: ""
+},
+2: {
+text: "Under whose leadership did Magadha come into prominence?",
+options: ["Bimbisara", "Ashoka", "Chandragupta Maurya", "Harsha"],
+correct: "1",
+image: ""
+},
+3: {
+text: "Who was a contemporary of Gautama Buddha?",
+options: ["Bimbisara", "Mahapadma Nanda", "Bindusara", "Harsha"],
+correct: "1",
+image: ""
+},
+4: {
+text: "Which king married princesses from Kosala, Lichchhavi, and Madra, aiding his expansionist policy?",
+options: ["Bimbisara", "Ajatashatru", "Chandragupta Maurya", "Harsha"],
+correct: "1",
+image: ""
+},
+5: {
+text: "Which territory did Bimbisara gain as dowry through marriage with the sister of King Prasenjit of Kosala?",
+options: ["Kashi", "Kosala", "Magadha", "Anga"],
+correct: "1",
+image: ""
+},
+6: {
+text: "Which region did Bimbisara conquer?",
+options: ["Anga", "Kashi", "Avanti", "Malwa"],
+correct: "1",
+image: ""
+},
+7: {
+text: "Which royal physician did Bimbisara send to Ujjain when Avanti King Pradyota was suffering from jaundice?",
+options: ["Jivaka", "Charaka", "Sushruta", "Dhanvantari"],
+correct: "1",
+image: ""
+},
+8: {
+text: "Which Indian king, known as Seniya, had the first regular and standing army?",
+options: ["Bimbisara", "Ashoka", "Chandragupta Maurya", "Harsha"],
+correct: "1",
+image: ""
+},
+9: {
+text: "Who built the city of New Rajagriha?",
+options: ["Bimbisara", "Ajatashatru", "Mahapadma Nanda", "Ashoka"],
+correct: "1",
+image: ""
+},
+// Questions
+
+10: {
+text: "Who succeeded Bimbisara as the ruler of Magadha?",
+options: ["Ajatashatru", "Bindusara", "Mahapadma Nanda", "Chandragupta Maurya"],
+correct: "1",
+image: ""
+},
+11: {
+text: "Ajatashatru gained complete control over which region by attacking his maternal uncle Prasenjit?",
+options: ["Kashi", "Anga", "Vaishali", "Kosala"],
+correct: "1",
+image: ""
+},
+12: {
+text: "Ajatashatru's next target after Kashi was which confederation?",
+options: ["Vajji", "Magadha", "Kosala", "Avanti"],
+correct: "1",
+image: ""
+},
+13: {
+text: "How many years did Ajatashatru's war with Vajji last, according to tradition?",
+options: ["16 years", "8 years", "22 years", "4 years"],
+correct: "1",
+image: ""
+},
+14: {
+text: "Who were Ajatashatru's diplomatic ministers who sowed discord amongst the Vajji people?",
+options: ["Sunidha and Vatsakar", "Chanakya and Katyayana", "Rakshasa and Pushyamitra", "Amatya and Dhaumya"],
+correct: "1",
+image: ""
+},
+15: {
+text: "What was Rathamusala, which played a role in defeating the Vajji?",
+options: ["A kind of chariot with a mace attached", "A royal palace", "A sacred river", "A type of fort"],
+correct: "1",
+image: ""
+},
+16: {
+text: "Which war engine, used by Ajatashatru, catapulted big stones?",
+options: ["Mahashilakantaka", "Dhanushka", "Gadadhara", "Chakraturni"],
+correct: "1",
+image: ""
+},
+17: {
+text: "Which two regions were added to Magadha's territory after Ajatashatru's conquest?",
+options: ["Kashi and Vaishali", "Anga and Kosala", "Avanti and Pataliputra", "Rajagriha and Ujjain"],
+correct: "1",
+image: ""
+},
+18: {
+text: "Which fort was built by Ajatashatru, along with a watch-fort called Jaladurg at Pataligrama?",
+options: ["Rajagriha fort", "Pataliputra fort", "Ujjain fort", "Vaishali fort"],
+correct: "1",
+image: ""
+},
+// Questions
+
+19: {
+text: "Who succeeded Ajatashatru as the ruler of Magadha?",
+options: ["Udayin", "Anuruddha", "Munda", "Naga-Dasak"],
+correct: "1",
+image: ""
+},
+20: {
+text: "Which city did Udayin lay the foundations for at the confluence of the Son and the Ganges?",
+options: ["Pataliputra", "Rajagriha", "Vaishali", "Kashi"],
+correct: "1",
+image: ""
+},
+21: {
+text: "Who succeeded Udayin as the rulers of Magadha?",
+options: ["Anuruddha, Munda, Naga-Dasak", "Ajatashatru, Bimbisara, Mahapadma Nanda", "Ashoka, Bindusara, Chandragupta Maurya", "Prasenjit, Chellana, Khema"],
+correct: "1",
+image: ""
+},
+
+}
